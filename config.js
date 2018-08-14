cf2731c1df9604cff4a8d4aaa61ebc1a293f4183
karancode <karanthanvi0@gmail.com>
2018-08-14 13:12:44 +0900

    remove token

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ module.exports = {
     slack_ws : process.env.slack_ws || 'pay-rakuten', //your workspace name goes here
     slack_token : process.env.slack_token || 'xoxp-XXXX', //your slack legacy token goes here
 
-    hipchat_token : process.env.hipchat_token || '89PnBYKmcJGSoJboyFclS0VqYfX6Zj4Oz70Rzbnk', //your hipchat token
+    hipchat_token : process.env.hipchat_token || 'XXXX', //your hipchat token
 
     hipchat_company : process.env.hipchat_company || 'XXXX' // your hipchat url domain name
 };
