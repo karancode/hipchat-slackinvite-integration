219fbd05548bbd80cdebc43246f6d45703a0cf22
karancode <karanthanvi0@gmail.com>
2018-08-15 01:11:27 +0900

    added checks & help features

--- app.js
+++ app.js
@@ -15,11 +15,31 @@ app.get('/', (req, res) => {
 
 app.post('/invite', (req, res) => {
 
-  let invite_email = modules.get_email(req.body.item.message.message);
   let hipchat_url = modules.get_hipchat_url(req.body.item.room.links.self);
+
+  console.log(req.body.item.message.message);
+  if (req.body.item.message.message === "/invite") {
+    modules.hipchat_callback(hipchat_url, "gray", "Please try '/invite help' for details!", config.hipchat_token);
+    res.send({ "Info": "Updated guide" });
+    console.log("only invite send");
+    return;
+  }
+
+  let invite_email = modules.get_email(req.body.item.message.message);
 
   if (!invite_email) {
     res.send({ 'Error': 'No email, No invite!' });
+  }
+  else if (invite_email === 'help') {
+    modules.hipchat_callback(hipchat_url, "gray", "Thanks for using hipchat-slackinvite-integration's help!\n You can get yourself invited using /invite <your_email_id> \n If anything goes wrong, please contact Admin! \n Details : https://github.com/kdthanvi/hipchat-slackinvite-integration",
+      config.hipchat_token);
+    res.send({ "Info": "Help Details" });
+    console.log("help sent");
+  }
+  else if (!invite_email.endsWith("@rakuten.com")) {
+    modules.hipchat_callback(hipchat_url, "gray", "Please use valid rakuten emailID!", config.hipchat_token);
+    res.send({ "Info": "Use rakuten email" });
+    console.log("use rakute emailID");
   }
   else {
     request.post({
@@ -31,7 +51,7 @@ app.post('/invite', (req, res) => {
       }
     }, function (err, httpResponse, body) {
       if (err) {
-        modules.hipchat_callback(hipchat_url, "red", err+": error res from slack api", config.hipchat_token);
+        modules.hipchat_callback(hipchat_url, "red", err + ": error res from slack api", config.hipchat_token);
         res.send({ 'Error': err });
         console.log('error res from slack-api');
         return;
@@ -45,17 +65,17 @@ app.post('/invite', (req, res) => {
       } else {          //return false otherwise
         let error = body.error;
         if (error === 'already_invited' || error === 'already_in_team') {
-          modules.hipchat_callback(hipchat_url, "yellow", error+": invitation not sent", config.hipchat_token);
+          modules.hipchat_callback(hipchat_url, "yellow", error + ": invitation not sent", config.hipchat_token);
           res.send({ 'Err': 'already invited' });
           console.log('err. alerday-there. response sent!');
         }
         else if (error === 'invalid_email') {
           res.send({ 'Err': 'invalid email' });
-          modules.hipchat_callback(hipchat_url, "yellow", error+": invitation not sent", config.hipchat_token);
+          modules.hipchat_callback(hipchat_url, "yellow", error + ": invitation not sent", config.hipchat_token);
           console.log('err. invalid-email. response sent!');
         }
         else if (error === 'invalid_auth') {
-          modules.hipchat_callback(hipchat_url, "red", error+': contact admin', config.hipchat_token);
+          modules.hipchat_callback(hipchat_url, "red", error + ': contact admin', config.hipchat_token);
           res.send({ 'Err ': 'un_authed. contact admin' });
           console.log('argh. un_authed. response sent!');
         }
